22a49e6509e6432a3cc49a1302413799abf5b0a3
Aaron Rincon <rlaaron0305@gmail.com>
2023-11-22 22:43:44 -0600

    add entity order

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService],
+  imports: [
+    TypeOrmModule.forFeature([Order])
+  ]
 })
 export class OrdersModule {}
